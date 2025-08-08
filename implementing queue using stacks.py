98a6abcfe283b9c10e7e67be57d3d0d5bc646f6b
Svaraan Kumar <svaraankumar.04@gmail.com>
2025-08-08 19:13:16 +0530

    Implementing Queue using Stacks done

---
+++ implementing queue using stacks.py
@@ -0,0 +1,23 @@
+class MyQueue:
+
+    def __init__(self):
+        self.inst = []
+        self.outst = []
+
+    def push(self, x: int) -> None:
+        self.inst.append(x)        
+
+    def pop(self) -> int:
+        if self.empty():
+            return -1
+        self.peek()
+        return self.outst.pop()
+
+    def peek(self) -> int:
+        if not self.outst:
+            while self.inst:
+                self.outst.append(self.inst.pop())
+        return self.outst[-1]        
+
+    def empty(self) -> bool:
+        return not self.inst and not self.outst
